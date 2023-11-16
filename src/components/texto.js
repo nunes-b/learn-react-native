162563129ca54ee9ca96b68bf2cf83f4473005b6
Rômulo Nunes <romulotg21@gmail.com>
2023-11-16 16:11:55 -0300

    feat: generic texto component

---
+++ src/components/texto.js
@@ -0,0 +1,22 @@
+import { Text, StyleSheet } from "react-native";
+
+export default function Texto({ children, style }) {
+  let estilo = estilos.texto;
+  if (style?.fontWeight === "bold") {
+    estilo = estilos.textoNegrito;
+  }
+
+  return <Text style={[style, estilo]}>{children}</Text>;
+}
+
+const estilos = StyleSheet.create({
+  texto: {
+    fontFamily: "Regular",
+    fontWeight: "normal",
+  },
+
+  textoNegrito: {
+    fontFamily: "Negrito",
+    fontWeight: "normal",
+  },
+});
